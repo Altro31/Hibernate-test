0444e0856dc30b5a92f2dcc6da82513cb158c71a
The Altro <albe020531@outlook.com>
2024-04-12 00:20:59 -0400

    another commit

--- src/main/java/org/altro/Main.java
+++ src/main/java/org/altro/Main.java
@@ -1,19 +1,38 @@
 package org.altro;
 
+import jakarta.persistence.*;
+import org.altro.schema.Author;
+import org.altro.schema.Book;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    private static EntityManagerFactory entityManagerFactory;
+
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        setup();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = null;
+        try (entityManager) {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(new Author("Altro", 21));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e;
         }
+    }
+
+    public static void setup() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("org.altro");
     }
 }
